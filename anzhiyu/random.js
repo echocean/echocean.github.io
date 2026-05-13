3348217bca0769389404b3e5c404c7e95326b7ec
fyfishie <fyfishie@outlook.com>
2026-05-13 14:13:13 +0800

    Site updated: 2026-05-13 14:13:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/10/Flow-Matching/","2025/06/14/use-of-AFF3CT/","2023/06/20/thread-safety-of-gorm/"];function toRandomPost(){
+var posts=["2025/10/10/Flow-Matching/","2026/05/13/ToolDNS/","2023/06/20/thread-safety-of-gorm/","2025/06/14/use-of-AFF3CT/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
