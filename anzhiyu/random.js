8598457422bd7a15dfae6f4366a2385ee417d824
fyfishie <fyfishie@outlook.com>
2026-08-10 11:20:43 +0800

    Remove obsolete Hexo assets

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2026/05/13/ToolDNS/","2025/10/10/Flow-Matching/","2024/04/25/alias-class/","2026/04/14/rich-arq/","2026/05/14/markov/","2023/06/20/thread-safety-of-gorm/","2025/06/14/use-of-AFF3CT/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
